2caf9cfb49880f40f63c90a62039b12c0aac4f61
wangmengqiang <842957445@qq.com>
2022-08-23 10:07:20 +0800

    master commit

--- src/main/java/ceshi.java
+++ src/main/java/ceshi.java
@@ -6,5 +6,6 @@ public class ceshi {
     public static void main(String[] args) {
         System.out.println("heiheihei");
         System.out.println("1111111111");
+        System.out.println("22222222");
     }
 }
