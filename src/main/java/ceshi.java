be16b4e6587029d52abf6dbcbc2bf48b07b42265
codewmq <90030428+codewmq@users.noreply.github.com>
2022-08-23 11:41:36 +0800

    Update ceshi.java

--- src/main/java/ceshi.java
+++ src/main/java/ceshi.java
@@ -8,5 +8,6 @@ public class ceshi {
         System.out.println("1111111111");
         System.out.println("2222222222");
         System.out.println("55555555555");
+        System.out.println("王梦强真帅啊！");
     }
 }
